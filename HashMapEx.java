892835549fbee65e179e0dba0970b49469f53153
Jaikumar Yadav <112802959+jkgit08@users.noreply.github.com>
2022-11-22 22:11:33 +0530

    Update HashMapEx.java

--- HashMapEx.java
+++ HashMapEx.java
@@ -1,8 +1,14 @@
+
 package hashMapEx;
 import java.util.*;
 public class hashMapEx{
    public static void main(String args[]){
      HashMap<String> map=new HashMap<String>();
-     map.add();
+      map.put("Apple ",477);
+      map.put("Cheery",799);
+      map.put("banana",80);
+      map.clear();      //Before all delete
+      map.put("Mango",100);
+      System.out.println(map);
    }
 }
