a9e296f6b23e8a11aa36039561745c47ee29bfb7
Jaikumar Yadav <112802959+jkgit08@users.noreply.github.com>
2022-11-22 22:02:47 +0530

    Create HashMapEx.java

---
+++ HashMapEx.java
@@ -0,0 +1,8 @@
+package hashMapEx;
+import java.util.*;
+public class hashMapEx{
+   public static void main(String args[]){
+     HashMap<String> map=new HashMap<String>();
+     map.add();
+   }
+}
