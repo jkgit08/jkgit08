a91891d46a862fd5cb01369fa561c407086964ef
Jaikumar Yadav <112802959+jkgit08@users.noreply.github.com>
2022-11-27 14:00:15 +0530

    Create Package.java

---
+++ Package.java
@@ -0,0 +1,23 @@
+//main class
+package pack_age;
+public class Pack_age {
+
+    public static void main(String[] args) {
+          student obj=new student();
+          obj.stu();
+    }
+}
+
+
+
+///class
+
+package pack_age;
+
+public class student {
+      public void stu(){
+           System.out.println("Student name is: Raghav");
+           System.out.println("Student roll no: 111");
+           System.out.println("Student age is: 21");
+     }
+}
