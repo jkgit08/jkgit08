b336c69a5ac7c62df9811f46a8f4bb2f50598bf2
Jaikumar Yadav <112802959+jkgit08@users.noreply.github.com>
2022-12-03 09:05:15 +0530

    Create Typecasting.java

---
+++ Typecasting.java
@@ -0,0 +1,15 @@
+package typecast;
+
+public class typecast{
+  public static void main(String args[]){
+      int a=12;
+      float b=(float)a;     
+      System.out.println(a);    //12
+      System.out.println(b);    //12.000
+    
+      float x=27.0806 f;
+      int i=(int)x;
+      System.out.println(x);     //27.0806
+      System.out.println(i);     //27
+  }
+}
