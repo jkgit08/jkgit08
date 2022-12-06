1e4f9932126b2026ed7413158f16da3d4a2564b5
Jaikumar Yadav <112802959+jkgit08@users.noreply.github.com>
2022-12-06 21:28:19 +0530

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -3,7 +3,7 @@ package bubblesort;
 public class BubbleSort{
    public static void main(String args[]){
      int arr[]={8,2,6,7,1,4,3,5};
-     
+     // bubble sort
      for(int i=0;i<arr.leanth;i++){
        for(int j=0;j<arr.leanth-1;j++){
          if(arr[j]>arr[j+1]){
@@ -15,7 +15,7 @@ public class BubbleSort{
      } 
        System.out.print("Output is :");
        for(int g=0;g<arr.leanth;g++){
-        System.out.print(arr[g]+" ");
+        System.out.print(arr[g]+" ");  //asending order print
        }
    }
 }
