2f4e309dcb465cbd21203685d402d4498c3b3813
Jaikumar Yadav <112802959+jkgit08@users.noreply.github.com>
2022-12-12 08:06:19 +0530

    Create DefaultConstructor.java

---
+++ DefaultConstructor.java
@@ -0,0 +1,21 @@
+package defaultconstructor;
+class bike1{
+      bike1(){
+          System.out.println("First Bike is created:");
+          System.out.println("Bike modul name is :"+"gs0345");          
+      }
+}
+class bike2{
+     bike2(){
+          System.out.println("Second Bike created:");
+          System.out.println("Bike modul name is:"+"hf8464");
+      }
+}
+public class Defaultconstructor {
+    public static void main(String[] args) {
+        bike1 b=new bike1();
+        bike2 b2=new bike2();
+                
+    }  
+}
+
